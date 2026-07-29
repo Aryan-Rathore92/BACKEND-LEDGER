e8c3fb28ec41fba50e3d694209947ddc77095b8d
Aryan-Rathore92 <rathorearyan533@gmail.com>
2026-07-29 21:37:00 +0530

    Check Balance

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -15,6 +15,26 @@ async function createAccountController(req, res){
 
 async function getUserAccountsController(req, res){
      
+    const accounts = await accountModel.find({ user: req.user._id});
+
+    res.status(200).json({
+        accounts
+    })
 }
 
-module.exports = {createAccountController, getUserAccountsController};
+async function getAccountBalanceController(req, res){
+           
+    const { accountId } = req.params;
+
+    const account = await accountModel.findOne({
+        _id: accountId,
+        user: req.user._id
+    });
+
+
+    if(!account){
+
+    }
+}
+
+module.exports = {createAccountController, getUserAccountsController, getAccountBalanceController};
